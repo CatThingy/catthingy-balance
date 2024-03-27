eefbc2fb02c663191836b0d83ebbf260b96b1fe8
CatThingy <andyc.andychen@gmail.com>
2024-03-26 23:46:17 -0700

    fix: Fix crossbow-adjacent bugs
    Flaming arrows were not checking for Flame, instead checking for Punch.
    Non-arrow damage was being ignored.

--- src/main/java/dev/catthingy/mixin/CrossbowBuffMixin.java
+++ src/main/java/dev/catthingy/mixin/CrossbowBuffMixin.java
@@ -24,7 +24,7 @@ public class CrossbowBuffMixin {
         if (punchLevel > 0) {
             abstractArrow.setKnockback(punchLevel);
         }
-        int flameLevel = EnchantmentHelper.getItemEnchantmentLevel(Enchantments.PUNCH_ARROWS, crossbowStack);
+        int flameLevel = EnchantmentHelper.getItemEnchantmentLevel(Enchantments.FLAMING_ARROWS, crossbowStack);
         if (flameLevel > 0) {
             abstractArrow.setSecondsOnFire(100);
         }
@@ -32,11 +32,11 @@ public class CrossbowBuffMixin {
 
     @Redirect(method = "performShooting", at=@At(value = "INVOKE", target = "Lnet/minecraft/world/item/CrossbowItem;shootProjectile(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/InteractionHand;Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/ItemStack;FZFFF)V", ordinal = 1))
     private static void shootMultishot1(Level level, LivingEntity shooter, InteractionHand hand, ItemStack crossbowStack, ItemStack ammoStack, float soundPitch, boolean isCreativeMode, float velocity, float inaccuracy, float projectileAngle, @Local float[] pitches, @Local boolean bl) {
-        CrossbowItemAccessor.callShootProjectile(level, shooter, hand, crossbowStack, ammoStack, pitches[1], bl, velocity, inaccuracy * 5.0F, 0.01F);
+        CrossbowItemAccessor.callShootProjectile(level, shooter, hand, crossbowStack, ammoStack, pitches[1], bl, velocity, inaccuracy * 7.5F, 0.01F);
     }
     @Redirect(method = "performShooting", at=@At(value = "INVOKE", target = "Lnet/minecraft/world/item/CrossbowItem;shootProjectile(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/InteractionHand;Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/ItemStack;FZFFF)V", ordinal = 2))
     private static void shootMultishot2(Level level, LivingEntity shooter, InteractionHand hand, ItemStack crossbowStack, ItemStack ammoStack, float soundPitch, boolean isCreativeMode, float velocity, float inaccuracy, float projectileAngle, @Local float[] pitches, @Local boolean bl) {
-        CrossbowItemAccessor.callShootProjectile(level, shooter, hand, crossbowStack, ammoStack, pitches[1], bl, velocity, inaccuracy * 5.0F, -0.01F);
+        CrossbowItemAccessor.callShootProjectile(level, shooter, hand, crossbowStack, ammoStack, pitches[1], bl, velocity, inaccuracy * 7.5F, -0.01F);
     }
 }
 @Mixin(CrossbowItem.class)
--- src/main/java/dev/catthingy/mixin/RemoveArrowInvulnMixin.java
+++ src/main/java/dev/catthingy/mixin/RemoveArrowInvulnMixin.java
@@ -2,6 +2,7 @@ package dev.catthingy.mixin;
 
 import net.minecraft.tags.DamageTypeTags;
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.damagesource.DamageTypes;
 import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Invoker;
@@ -10,13 +11,16 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(LivingEntity.class)
 public class RemoveArrowInvulnMixin {
-    @Redirect(method = "hurt", at=@At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;actuallyHurt(Lnet/minecraft/world/damagesource/DamageSource;F)V", ordinal = 1))
+    @Redirect(method = "hurt", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;actuallyHurt(Lnet/minecraft/world/damagesource/DamageSource;F)V", ordinal = 1))
     void invulnHurt(LivingEntity instance, DamageSource damageSource, float damageAmount) {
-        if (instance.invulnerableTime > 10.0 && damageSource.is(DamageTypeTags.BYPASSES_COOLDOWN)) {
-            ((LivingEntityAccessor)instance).callActuallyHurt(damageSource, damageAmount * 0.5F);
+        if (instance.invulnerableTime > 10.0 && damageSource.is(DamageTypeTags.BYPASSES_COOLDOWN) && damageSource.is(DamageTypes.ARROW)) {
+            ((LivingEntityAccessor) instance).callActuallyHurt(damageSource, damageAmount * 0.5F);
+        } else {
+            ((LivingEntityAccessor) instance).callActuallyHurt(damageSource, damageAmount);
         }
     }
 }
+
 @Mixin(LivingEntity.class)
 interface LivingEntityAccessor {
     @Invoker
