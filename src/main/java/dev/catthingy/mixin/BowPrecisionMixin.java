dd639ff3e0586c2e1b6afb62755dae2e0da62fa8
CatThingy <andyc.andychen@gmail.com>
2024-03-26 23:24:26 -0700

    feat: Require precision when using bows
    Bows only crit within a 2-tick window after being fully charged.

---
+++ src/main/java/dev/catthingy/mixin/BowPrecisionMixin.java
@@ -0,0 +1,21 @@
+package dev.catthingy.mixin;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.projectile.AbstractArrow;
+import net.minecraft.world.item.BowItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(BowItem.class)
+public class BowPrecisionMixin {
+    @Inject(method = "releaseUsing", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;addFreshEntity(Lnet/minecraft/world/entity/Entity;)Z"))
+    void bowPrecision(ItemStack stack, Level level, LivingEntity livingEntity, int timeCharged, CallbackInfo ci, @Local AbstractArrow arrow, @Local float f) {
+        int chargeTime = ((BowItem) ((Object) this)).getUseDuration(stack) - timeCharged;
+        arrow.setCritArrow(chargeTime > 20 && chargeTime <= 22);
+    }
+}
